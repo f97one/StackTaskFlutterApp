0053be957523337255e589f1159098de8c5430d8
HAJIME Fukuna <f97one@gmail.com>
2019-01-02 16:57:56 +0900

    基底クラスの仕様を変えることになった件に対応。

--- lib/screen/abstract_app_screen_state.dart
+++ lib/screen/abstract_app_screen_state.dart
@@ -2,11 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:stack_task/datasource/firebase_auth_datasource.dart';
 
-abstract class AbstractAppScreen extends StatelessWidget {
-
-  final String title;
-
-  AbstractAppScreen({Key key, this.title}) : super(key: key);
+abstract class AbstractAppScreenState<T extends StatefulWidget> extends State<T> {
 
   Future<FirebaseUser> currentUser() async {
     return await FirebaseAuthDatasource().currentUser();
--- lib/screen/task_list_screen.dart
+++ lib/screen/task_list_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:stack_task/screen/abstract_app_screen.dart';
+import 'package:stack_task/screen/abstract_app_screen_state.dart';
 
-class TaskListScreen extends AbstractAppScreen {
+class TaskListScreen extends StatefulWidget {
 
-  TaskListScreen({Key key, String title}) : super(key: key, title: title);
+  TaskListScreen({Key key}) : super(key: key);
+
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+
+}
+
+class _TaskListScreenState extends AbstractAppScreenState<TaskListScreen> {
+
+  final String title = 'Task List';
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +31,7 @@ class TaskListScreen extends AbstractAppScreen {
   }
 
   void _addNewTask() {
-
+    Navigator.of(context).pushNamed('/taskadd');
   }
+
 }
