4cf7de080cd95ac0b04e08bedaf523943b9e70a3
HAJIME Fukuna <f97one@gmail.com>
2019-01-19 11:36:50 +0900

    ひとまずログイン処理実装。

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -7,7 +7,6 @@ import 'package:stack_task/datasource/firebase_auth_datasource.dart';
 import 'package:stack_task/screen/task_list_screen.dart';
 
 class LoginScreen extends StatefulWidget {
-
   final String title;
 
   LoginScreen({Key key, this.title}) : super(key: key);
@@ -23,45 +22,38 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _handleScreen(context),
+      body: _buildLoginScreen(context),
     );
   }
 
-  Widget _handleScreen(BuildContext context) {
-    FirebaseAuthDatasource datasource = FirebaseAuthDatasource();
-
-    if (datasource.currentUser() != null) {
-      Navigator.of(context).pushReplacementNamed('/tasklist');
-    }
-
+  Widget _buildLoginScreen(BuildContext context) {
     return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child:
-                  RaisedButton(
-                    key:null,
-                    onPressed: _requestLoginWithGoogle,
-                    color: const Color(0xFF0099ed),
-                    child: Text(
-                      "Login with Google",
-                      style: TextStyle(
-                        fontSize:12.0,
-                        color: const Color(0xFFffffff),
-                        fontWeight: FontWeight.w200,
-                        fontFamily: "Roboto"
-                      )
-                    )
-                  ),
-              )
-            ],
-          );
-  }
-
-  void _requestLoginWithGoogle() {
-    FirebaseUser user = FirebaseAuthDatasource().handleSignIn();
-    if (user != null) {
-      
-    }
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: RaisedButton(
+              key: null,
+              onPressed: () {
+                FirebaseAuthDatasource().handleSignIn().then((user) {
+                  if (user != null) {
+                    Navigator.pushReplacementNamed(context, '/tasklist');
+                  }
+                }).catchError((onError) {
+                  Fluttertoast.showToast(
+                      msg: 'Authentication Failed, Please retry.',
+                      gravity: ToastGravity.BOTTOM,
+                      toastLength: Toast.LENGTH_SHORT);
+                });
+              },
+              color: const Color(0xFF0099ed),
+              child: Text("Login with Google",
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color: const Color(0xFFffffff),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"))),
+        )
+      ],
+    );
   }
 }
