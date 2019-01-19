fed0c2bbb7d4e50cb20d9ec9f586a5290e487e5c
HAJIME Fukuna <f97one@gmail.com>
2019-01-19 11:40:54 +0900

    FutureBuilderでFirebaseUserを使う方式に修正。

--- lib/screen/abstract_app_screen_state.dart
+++ lib/screen/abstract_app_screen_state.dart
@@ -4,38 +4,57 @@ import 'package:stack_task/datasource/firebase_auth_datasource.dart';
 
 abstract class AbstractAppScreenState<T extends StatefulWidget> extends State<T> {
 
-  Future<FirebaseUser> currentUser() async {
-    return await FirebaseAuthDatasource().currentUser();
-  }
-
   /**
    * NavigationDrawerを組み立てる。
-   * 
-   * @param context 
+   *
+   * @param context
    */
   Widget createDrawer(BuildContext context) {
-    FirebaseUser user = currentUser() as FirebaseUser;
-    
+    return FutureBuilder<FirebaseUser>(
+      future: FirebaseAuthDatasource().currentUser(),
+      builder: (BuildContext ctx, AsyncSnapshot<FirebaseUser> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return _makeDrawer(context, snapshot.data);
+        }
+      },
+    );
+  }
+
+  PreferredSizeWidget createDefaultAppBar(String title) {
+    return AppBar(
+      title: Text(title),
+    );
+  }
+
+  Drawer _makeDrawer(BuildContext context, FirebaseUser firebaseUser) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.grey
-            ),
+            decoration: BoxDecoration(color: Colors.grey),
             child: Row(
               children: <Widget>[
-                Image.network(user.photoUrl),
-                Text(user.displayName),
-                Text(user.email)
+                Image.network(firebaseUser.photoUrl),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(4.0, 16.0, 4.0, 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(firebaseUser.displayName),
+                      Text(firebaseUser.email),
+                    ],
+                  ),
+                ),
               ],
-            )
+            ),
           ),
           ListTile(
             title: Text('Task List'),
             onTap: () {
               // ToDo : タスクリスト画面への遷移を書く
+//              Navigator.of(context).pushReplacementNamed('/tasklist');
+              debugPrint('タスクリストを押した');
               Navigator.pop(context);
             },
           ),
@@ -43,6 +62,7 @@ abstract class AbstractAppScreenState<T extends StatefulWidget> extends State<T>
             title: Text('Setting'),
             onTap: () {
               // ToDo : アプリ設定画面への遷移を書く
+              debugPrint('設定を押した');
               Navigator.pop(context);
             },
           ),
@@ -51,6 +71,7 @@ abstract class AbstractAppScreenState<T extends StatefulWidget> extends State<T>
             title: Text('Logout'),
             onTap: () {
               // ToDo : ログアウト要求を書く
+              debugPrint('ログアウトを押した');
               Navigator.pop(context);
             },
           )
