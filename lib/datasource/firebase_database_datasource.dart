c1083478496193085b111f9d6bbcbf308be4c03a
HAJIME Fukuna <f97one@gmail.com>
2019-01-21 19:13:50 +0900

    ひとまず1回取得処理を仮実装。

---
+++ lib/datasource/firebase_database_datasource.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:firebase_database/firebase_database.dart';
+
+class FirebaseDatabaseDatasource {
+  DatabaseReference _mainReference;
+
+  final String _referenceTag = 'StackTask';
+
+  FirebaseDatabaseDatasource() {
+    _mainReference = FirebaseDatabase.instance.reference().child(_referenceTag);
+  }
+
+  Future<DataSnapshot> readTasksOnce(String uid) async {
+    return await _mainReference.child(uid).once();
+  }
+}
