24a042e88f84742591e62e9e325902760d7f2ecb
HAJIME Fukuna <f97one@gmail.com>
2019-02-05 20:54:13 +0900

    ひとまず1件更新を実装。

--- lib/datasource/firebase_database_datasource.dart
+++ lib/datasource/firebase_database_datasource.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:stack_task/kind/task_item.dart';
 
 class FirebaseDatabaseDatasource {
   DatabaseReference _mainReference;
+
+  /// このアプリで使う DatabaseReference.
+  DatabaseReference get reference => _mainReference;
 
   final String _referenceTag = 'StackTask';
 
@@ -14,5 +18,22 @@ class FirebaseDatabaseDatasource {
     return await _mainReference.child(uid).once();
   }
 
-  DatabaseReference getReference() => _mainReference;
+  /// タスクをRealtime Databaseに送る。
+  ///
+  /// * taskItem : 送るタスク情報
+  /// * uid : ユーザーのUID
+  /// * isCreation : 追加の時true、更新の時false、デフォルトfalse
+  Future<void> putTask({TaskItem taskItem, String uid, bool isCreation = false}) async {
+    var ref = reference.child(uid);
+
+    taskItem.updatedAt = DateTime.now();
+
+    if (isCreation) {
+      taskItem.taskId = ref.push().key;
+      taskItem.createdAt = DateTime.now();
+    }
+    Map<String, dynamic> updateMap = {taskItem.taskId: taskItem.toJson()};
+
+    await ref.update(updateMap);
+  }
 }
